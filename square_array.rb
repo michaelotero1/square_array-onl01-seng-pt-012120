f939106f58c9090665083037278c2ba424ac8b15
michael otero <michaelotero1118@gmail.com>
2020-02-01 21:55:45 +0000

    Automatically backed up by Learn

--- square_array.rb
+++ square_array.rb
@@ -4,8 +4,19 @@
 
 
 def square_array(array)
+<<<<<<< HEAD
  x = []
  array.each {|y| x << y ** 2}
  x
+=======
+  new_array = []
+  counter = 0
+
+ while array[counter]do
+   new_array << array[counter] **2
+  end
+   return new_array 
+  
+>>>>>>> 6171840ff3ce104dbe3e7342bfce63c75a73a60d
 end
 #.each{|brother| puts "Stop hitting yourself #{brother}!"
